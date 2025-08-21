361d43029fd2d9780ec336cc4a54129499410ff5
seraj <serajstudy1@gmail.com>
2025-08-20 22:42:29 -0400

    commit

---
+++ javascript.mjs
@@ -0,0 +1,42 @@
+// const adventurer = {
+// name: "Robin",
+// health: 10,
+// inventory: ["sword", "potion", "artifact"],
+// companion: {
+//     name: "Leo",
+//     type: "Cat"
+// }
+// }
+
+// From the adventurer object, we can access Robin’s inventory using a combination 
+
+
+
+// console.log(adventurer.name);
+// console.log(adventurer.inventory);
+// console.log(adventurer.inventory[0]);
+
+
+// create a loop that logs each item in Robin’s inventory.
+
+const adventurer = {
+name: "Robin",
+health: 10,
+inventory: ["sword", "potion", "artifact"],
+companion: {
+    name: "Leo",
+    type: "Cat"
+}
+
+
+}
+
+// for (let item of adventurer.inventory) {
+//   console.log(item);
+// }
+
+
+console.log(adventurer.companion.name);
+
+
+
